746172233215487c79d8a0ad97977b6cbed6a58f
egiginting10 <78341282+egiginting10@users.noreply.github.com>
2022-04-20 18:53:16 +0700

    Add files via upload

---
+++ UG-8_71200634/src/Lina.java
@@ -0,0 +1,14 @@
+public class Lina {
+    public Lina(){
+        String name="Lina";
+        int health = 800;
+        int damage = 70;
+        int mana = 120;
+    }
+    public boolean skill(Character){
+        int damage = 600;
+    }
+    public void attack(Character){
+
+    }
+}
