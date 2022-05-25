fee6f589f7f08c31b63e483f813881f081487a91
egiginting10 <78341282+egiginting10@users.noreply.github.com>
2022-05-25 18:53:35 +0700

    Add files via upload

---
+++ UG12_71200634/UG12-71200634-ProjectKassa/src/main/java/Kassa.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Kassa {
+    private HashMap<Produk, Integer> pesanan = new HashMap<Produk, Integer>();
+    private ArrayList<Kasir> arrKasir = new ArrayList<Kasir>();
+    private Kasir kasir;
+
+    public Kassa(){
+    }
+
+    public void login(String username, String password){
+
+    }
+    public void register(Kasir kasir){
+
+    }
+    public void tambahpesanan(Produk produk, int jumlah){
+
+    }
+    public void printNota(){
+
+    }
+    public void printPenjualanKasir(){
+
+    }
+
+    public void tambahPesanan(Produk produk2, int i) {
+    }
+}
